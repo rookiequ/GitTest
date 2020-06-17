c7b0e60a445ba149679c43de3f58bd1f1a043981
Zhouziqin <414769606@qq.com>
2020-06-17 17:41:19 +0800

    git commit

--- src/com/git/GitTest.java
+++ src/com/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git的简单使用");
         System.out.println("将文件改为2.0版本");
+        System.out.println("git冲突");
     }
 }
